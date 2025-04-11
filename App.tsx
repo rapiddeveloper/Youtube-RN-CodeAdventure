9129aa989a7a75429463423e2f1f0186b1ba479d
rapiddeveloper <77062058+rapiddeveloper@users.noreply.github.com>
2025-04-10 19:16:07 -0700

    tab navigation setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import YoutubeTabStack from './src/navigation/YoutubeTabStack';
+
+
+const RootStack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+         <RootStack.Navigator screenOptions={{headerShown: false}}>
+            <RootStack.Screen name='YoutubeTabs' component={YoutubeTabStack} />
+         </RootStack.Navigator>
+    </NavigationContainer>
+   
   );
 }
 
